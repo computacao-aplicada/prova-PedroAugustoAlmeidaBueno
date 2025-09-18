ee8c4f9b22cec333203c858c9f5c296a026af3aa
Pedro <pedro.bueno@edu.unifil.br>
2025-09-17 21:54:53 -0300

    passo 5 e 6 certo

--- src/Validador.java
+++ src/Validador.java
@@ -5,7 +5,7 @@ public class Validador {
 
             return false;
         }
-        String limpo = cpf.trim().replaceAll("[.-]", "");
+        String limpo = cpf.trim().replaceAll("[.\\-]", "");
         if (!limpo.matches("\\d{11}")) {
             return false;
         }
@@ -13,8 +13,22 @@ public class Validador {
             return false;
         }
         // TODO: calcular DV
-        return true;
+        return checarDigitos(limpo);
 
     }
-
+    private static boolean checarDigitos(String cpf) {
+        int[] d = cpf.chars().map(c -> c - '0').toArray();
+        // DV1
+        int s1 = 0;
+        for (int i = 0; i < 9; i++) s1 += d[i] * (10 - i);
+        int r1 = s1 % 11;
+        int dv1 = (r1 < 2) ? 0 : 11 - r1;
+        if (d[9] != dv1) return false;
+        // DV2
+        int s2 = 0;
+        for (int i = 0; i < 10; i++) s2 += d[i] * (11 - i);
+        int r2 = s2 % 11;
+        int dv2 = (r2 < 2) ? 0 : 11 - r2;
+        return d[10] == dv2;
+    }
 }
