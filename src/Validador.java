076985ab13fc4ac90c14cca86675af5d87db02a0
Pedro <pedro.bueno@edu.unifil.br>
2025-09-17 21:20:56 -0300

    1 passo

--- src/Validador.java
+++ src/Validador.java
@@ -3,6 +3,6 @@ public class Validador {
     public static boolean validarCPF(String cpf) {
         return false;
     }
-
+ streaming pedroskds
 }
 
--- tests/ValidadorTest.java
+++ tests/ValidadorTest.java
@@ -4,4 +4,9 @@ import org.junit.jupiter.api.Test;
 
 
 public class ValidadorTest {
+    @Test
+    void deveValidarCPFValido() {
+        assertTrue(Validador.validarCPF("529.982.247-25"));
+        assertTrue(Validador.validarCPF("52998224725"));
+    }
 }
